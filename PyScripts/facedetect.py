421f0e2ec23a42320949961df4ddd21a464833e4
Prassanna <atemysemicolon@gmail.com>
2015-09-22 22:54:14 +0200

    Updated styles2

--- PyScripts/facedetect.py
+++ PyScripts/facedetect.py
@@ -1,34 +1,28 @@
 import cv2
+from skimage import data
 import sys
 
 cascPath = sys.argv[1]
 faceCascade = cv2.CascadeClassifier(cascPath)
 
-video_capture = cv2.VideoCapture(0)
+frame = data.lena();
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30)
+)
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30)fafa
-    )
+# Display the resulting frame
+cv2.imshow('Video', frame)
 
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+cv2.waitkey()
 
 # When everything is done, release the capture
 video_capture.release()
-cv2.destroyAllWindows()
